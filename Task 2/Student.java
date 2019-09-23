cc38e79ad80a37f54e8b95e239af90f57b268482
Editux <43064329+Editux@users.noreply.github.com>
2019-09-23 21:18:54 +0300

    Create Student.java

---
+++ Task 2/Student.java
@@ -0,0 +1,64 @@
+package com.company;
+
+
+
+public class Student {
+    private String name;
+    private String surname;
+    private String group;
+
+
+
+   Subject[] subject= new Subject[10];
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setSubject(Subject[] subject) {
+      this.subject=subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public Subject[] getSubject() {
+        return subject.clone();
+    }
+
+    public double getAverage(int size) {
+        float sum_a=0;
+        float sum_b=0;
+
+        double average;
+
+
+        for (int i = 0; i < size; i++) {
+
+
+
+            sum_a += sum_a + subject[i].getGrade();
+            sum_b += sum_b+ subject[i].getCredits(); ;
+
+        }
+        average =sum_a *sum_b/sum_b;
+        return average / 10;
+    }
+}
